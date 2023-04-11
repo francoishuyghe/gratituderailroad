a60fe3a23061c90c799afc183de14b1c71d74efd
Francois Huyghe <francois@13millieconds.com>
2023-04-11 16:58:32 -0400

    Images and blog page

--- web/app/themes/gratitude2023/resources/views/index.blade.php
+++ web/app/themes/gratitude2023/resources/views/index.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
-
   @if (! have_posts())
     <x-alert type="warning">
       {!! __('Sorry, no results were found.', 'sage') !!}
@@ -11,9 +9,22 @@
     {!! get_search_form(false) !!}
   @endif
 
+  @php 
+  $page_for_posts_id = get_option( 'page_for_posts' );
+  echo apply_filters( 'the_content', get_post_field( 'post_content', $page_for_posts_id ) );
+  @endphp
+
+<section id="allPosts">
+  <div class="container">
+  <div class="row">
   @while(have_posts()) @php(the_post())
-    @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+    <div class="col-md-4">
+      @includeFirst(['partials.content-' . get_post_type(), 'partials.content'])
+    </div>
   @endwhile
+    </div>
+  </div>
+</section>
 
   {!! get_the_posts_navigation() !!}
 @endsection
