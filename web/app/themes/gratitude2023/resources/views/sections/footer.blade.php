79347b38e85871762b550d7d9fcf6950ac986aec
Francois Huyghe <francois@13millieconds.com>
2023-04-19 14:27:02 -0400

    Footer styling

--- web/app/themes/gratitude2023/resources/views/sections/footer.blade.php
+++ web/app/themes/gratitude2023/resources/views/sections/footer.blade.php
@@ -1,7 +1,15 @@
 <footer class="content-info">
-  <div class="container">
+  <div class="primary">
     <div class="row">
       @php(dynamic_sidebar('sidebar-footer'))
+    </div>
+  </div>
+  <div class="secondary">
+    <div class="copyright">
+      {{ the_time('Y') }} {!! $siteName !!}. All rights reserved.
+    </div>
+    <div class="menu">
+      @php(dynamic_sidebar('sidebar-secondary-footer'))
     </div>
   </div>
 </footer>
