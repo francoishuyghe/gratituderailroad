3d22f55717cba2997ccfbee764718aa4a223d373
Francois Huyghe <francois@13millieconds.com>
2023-04-20 13:28:24 -0400

    basic footer layout

--- web/app/themes/gratitude2023/resources/views/sections/footer.blade.php
+++ web/app/themes/gratitude2023/resources/views/sections/footer.blade.php
@@ -25,4 +25,13 @@
       </nav>
     </div>
   </div>
+  <div class="secondary">
+    <div class="copyright">
+      {{ the_time('Y') }} {!! $siteName !!}. All rights reserved.
+    </div>
+    <div class="menu">
+      @php(dynamic_sidebar('sidebar-secondary-footer'))
+    </div>
+  </div>
+
 </footer>
