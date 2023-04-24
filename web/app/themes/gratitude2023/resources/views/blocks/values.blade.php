2e1d4608c169d5176a52bc748db4a6909254827e
Francois Huyghe <francois@13millieconds.com>
2023-04-24 11:57:37 -0400

    values section

--- web/app/themes/gratitude2023/resources/views/blocks/values.blade.php
+++ web/app/themes/gratitude2023/resources/views/blocks/values.blade.php
@@ -1,15 +1,21 @@
 <div class="{{ $block->classes }}">
-  @if ($items)
-    <ul>
-      @foreach ($items as $item)
-        <li>{{ $item['item'] }}</li>
+  <div class="container">
+    @if ($values)
+    <div class="row justify-content-md-center">
+        @foreach ($values as $value)
+  
+        <div class="value col-md-4">
+          @php $image = $value['image'] @endphp
+          @if( !empty( $image ) )
+            <img src="<?php echo esc_url($image['sizes']['medium']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+          @endif
+          <h3>{{ $value['name'] }}</h3>
+          <p>{{ $value['description'] }}</p>
+        </div>  
       @endforeach
-    </ul>
+      </div>
   @else
     <p>{{ $block->preview ? 'Add an item...' : 'No items found!' }}</p>
   @endif
-
-  <div>
-    <InnerBlocks />
-  </div>
+</div>
 </div>
