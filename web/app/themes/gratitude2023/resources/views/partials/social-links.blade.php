ecd8d6fe6c3526a21b5bec0c4bd13b9ab3c655d0
Francois Huyghe <francois@13millieconds.com>
2023-07-14 16:23:19 -0400

    fixed footer link

--- web/app/themes/gratitude2023/resources/views/partials/social-links.blade.php
+++ web/app/themes/gratitude2023/resources/views/partials/social-links.blade.php
@@ -1,4 +1,4 @@
 <div id="socialLinks">
     <a href="{{ $social['linkedin_link'] }}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-    <a href="{{ $social['linkedin_link'] }}" target="_blank"><i class="fa-brands fa-twitter"></i></a>
-  </div>
+    <a href="{{ $social['twitter_link'] }}" target="_blank"><i class="fa-brands fa-twitter"></i></a>
+</div>
