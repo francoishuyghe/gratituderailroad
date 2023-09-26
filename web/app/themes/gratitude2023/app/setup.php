c2b84351baf2490df85bf0b898f4796a839dc2ab
Francois Huyghe <francois@13milliseconds.com>
2023-09-26 15:13:07 -0400

    decode the js on

--- web/app/themes/gratitude2023/app/setup.php
+++ web/app/themes/gratitude2023/app/setup.php
@@ -737,7 +737,7 @@ function add_email_to_newsletter(){
 
   wp_send_json_success([
     'http_code' => curl_getinfo($ch, CURLINFO_HTTP_CODE), 
-    'data' => $result
+    'data' => json_decode($result)
   ]);
 }
 add_action('wp_ajax_add_email_to_newsletter', __NAMESPACE__ . '\\add_email_to_newsletter');
