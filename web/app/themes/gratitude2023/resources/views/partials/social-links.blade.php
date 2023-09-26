9ebab0ebc0cc62f70dc24bca4b856f9b708cb397
Francois Huyghe <francois@13milliseconds.com>
2023-09-26 14:58:47 -0400

    Design edits

--- web/app/themes/gratitude2023/resources/views/partials/social-links.blade.php
+++ web/app/themes/gratitude2023/resources/views/partials/social-links.blade.php
@@ -1,4 +1,3 @@
 <div id="socialLinks">
     <a href="{{ $social['linkedin_link'] }}" target="_blank"><i class="fa-brands fa-linkedin"></i></a>
-    <a href="{{ $social['twitter_link'] }}" target="_blank"><i class="fa-brands fa-twitter"></i></a>
 </div>
