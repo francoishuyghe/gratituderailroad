9d0f7ed01458dd0cc63a1b7c78a350cb185fbedd
Francois Huyghe <francois@13milliseconds.com>
2023-10-08 21:24:42 -0400

    Latest news and footer edits

--- web/app/themes/gratitude2023/resources/views/blocks/latest-news.blade.php
+++ web/app/themes/gratitude2023/resources/views/blocks/latest-news.blade.php
@@ -2,20 +2,20 @@
 
 <div class="{{ $block->classes }}">
   <div class="container swiper-container" data-slides="3">
-    <header>
       <h3>{{ $title }}</h3>
-      <a class="round-button orange" href="/insights">All News</a>
-    </header>
       <div class="swiper-wrapper">
-      @if(!empty($news))
+        @if(!empty($news))
         @foreach ($news as $post)
-          @php setup_postdata($post) @endphp
-          <div class="swiper-slide">
-            @include('partials.content')
-          </div>
-          @php wp_reset_postdata() @endphp
+        @php setup_postdata($post) @endphp
+        <div class="swiper-slide">
+          @include('partials.content')
+        </div>
+        @php wp_reset_postdata() @endphp
         @endforeach
-      @endif
-    </div>
+        @endif
+      </div>
+      <div class="link">
+        <a class="round-button orange" href="/insights">All News</a>
+      </div>
   </div>
 </div>
