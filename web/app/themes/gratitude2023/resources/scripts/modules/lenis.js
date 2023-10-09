7d8e551c32773af1b49e9aecc56a844a51bbaa7d
Francois Huyghe <francois@13milliseconds.com>
2023-10-08 21:32:51 -0400

    Add responsive variables for animations

--- web/app/themes/gratitude2023/resources/scripts/modules/lenis.js
+++ web/app/themes/gratitude2023/resources/scripts/modules/lenis.js
@@ -33,36 +33,54 @@ if( window.innerHeight > 800 ){
 
   // Stacked illustrations
   const stackedIllustrations = document.querySelectorAll('.wp-block-image.stacked')
+
+  let mm = gsap.matchMedia(),
+  breakPoint = 800;
+
+  mm.add({
+
+    // set up any number of arbitrarily-named conditions. The function below will be called when ANY of them match.
+    isDesktop: `(min-width: ${breakPoint}px)`,
+    isMobile: `(max-width: ${breakPoint - 1}px)`,
+
+  }, (context) => {
+
+    // context.conditions has a boolean property for each condition defined above indicating if it's matched or not.
+    let { isDesktop, isMobile } = context.conditions;
+
     stackedIllustrations.forEach(stack => {
       
-        gsap.timeline({
-          scrollTrigger: {
-            trigger: stack,
-            start: 'top bottom',
-            end: 'top center',
-            scrub: true,
-        },
-        })
-        .from(stack, {
-          y: 50,
-        })
-        .to(stack, {
-          ease: 'none',
-          '--movement': 25,
-          y: 0,
-        })
-    
-      });
+      gsap.timeline({
+        scrollTrigger: {
+          trigger: stack,
+          start: 'top bottom',
+          end: 'top center',
+          scrub: true,
+      },
+      })
+      .from(stack, {
+        y: isDesktop ? 50 : 24,
+      })
+      .to(stack, {
+        ease: 'none',
+        '--movement': isDesktop? 25 : 12,
+        y: 0,
+      })
+  
+    });
 
-      // Post Cards
-      ScrollTrigger.batch('.wp-block-latest-news .post-card', {
-        onEnter: elements => {
-          gsap.from(elements, {
-            y: 60,
-            stagger: 0.15
-          });
-        },
-        start: 'top bottom',
-        end: 'top center',
-        scrub: true,
-      });
+    // Post Cards
+    ScrollTrigger.batch('.wp-block-latest-news .post-card', {
+      onEnter: elements => {
+        gsap.from(elements, {
+          y: isDesktop ? 50 : 20,
+          stagger: 0.15
+        });
+      },
+      start: 'top bottom',
+      end: 'top center',
+      scrub: true,
+    });
+
+  }); 
+    
